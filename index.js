43ad80733bc4554749fe834b5742aa5971ce538a
ThomasVancampen <79362643+ThomasVancampen@users.noreply.github.com>
2022-02-22 22:03:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://cocky-snyder-5a02cf.netlify.app'
+        origin: '*'
     })
 );
 app.options('*', cors());
